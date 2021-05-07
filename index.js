d72fde34a8590c082c1de8e27110e8088195b48a
Gerald Dino <dino.gerald108@gmail.com>
2021-05-07 13:20:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ function logWhisper(string) {
  }
 
  function sayHiToGrandma(string) {
-   if (string.toLowerCase(string) === string) {
+   if (string.toLowerCase() === string) {
      return 'I can\'t hear you!';
-   } else if (string.toUpperCase(string) === string) {
+   } else if (string.toUpperCase() === string) {
      return 'YES INDEED!';
    } else if (string === 'I love you, Grandma.') {
      return 'I love you, too.';
